6ec944784618dc47cd052c383d317201c87919b8
ivan.mihaylovskyy <edongrossi1@gmail.com>
2015-06-03 07:10:05 +0300

    change assembly name; update usage; update nuspec file

--- NUnitReporter/Properties/AssemblyInfo.cs
+++ NUnitReporter/Properties/AssemblyInfo.cs
@@ -1,9 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 using CommandLine;
 
 [assembly: AssemblyTitle("NUnitReporter")]
@@ -15,28 +11,14 @@ using CommandLine;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4c567c1d-1959-487d-b42e-f745b235ede1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyUsage(
-    "NUnitReporter.exe TestResult.xml",
-    "NUnitReporter.exe TestResult.xml -o report",
-    "NUnitReporter.exe TestResult.xml -a testoutput -o report"
-)]
+    "nure.exe TestResult.xml",
+    "nure.exe TestResult.xml -o path/to/generated/report",
+    "nure.exe TestResult.xml -a path/to/screenshots -o path/to/generated/report"
+    )]
