dfb2e6c3140533e79b6921efc85210aa2676a3ed
AlexGalchenko <alexey.galchenko@justanswer.com>
2016-11-24 14:13:48 +0200

    Update UI filtering

--- templates/default/js/controllers/DashboardController.js
+++ templates/default/js/controllers/DashboardController.js
@@ -1,8 +1,13 @@
 var RESULT_TYPE_ANY = "Any";
 
 function DashboardController($scope, $location, $routeParams, StateStorage, reportAdapter) {
-    $scope.testCaseFilter = function(testCase) {
+	
+	$scope.testCaseFilter = function(testCase) {
         return $scope.isResultTypeActive(testCase.result) || $scope.isResultTypeAny();
+    }
+    $scope.textFilter = function(testCase) {
+        matches = testCase.name.indexOf($scope.query) !== -1 || testCase.getMessage().indexOf($scope.query) !== -1
+        return $scope.excludeFilter ? !matches : matches;    
     }
 
     $scope.isResultTypeActive = function(resultType) {
@@ -25,7 +30,10 @@ function DashboardController($scope, $location, $routeParams, StateStorage, repo
         return StateStorage.get('ActiveResultType') || RESULT_TYPE_ANY;
     };
 
+    $scope.getlistbyMessage 
     $scope.openTestCase = function(testCase) {
+        StateStorage.put('query', $scope.query);
+        StateStorage.put('exclude', $scope.excludeFilter);
         $location.path('testcase/' + testCase.id);
     };
 
@@ -48,7 +56,11 @@ function DashboardController($scope, $location, $routeParams, StateStorage, repo
 
     $scope.currentTestCase = $routeParams.id 
         ? reportAdapter.findTestCaseById($routeParams.id)
-        : $scope.testCases[0];
+        : $scope.testCases[0]; 
+
+    $scope.query = StateStorage.get('query') || '';
+    $scope.excludeFilter = StateStorage.get('exclude') || false;
+    console.log($scope.query);
 }
 
 module.exports = ['$scope', '$location', '$routeParams', 'StateStorage', 'ReportAdapter', DashboardController];
